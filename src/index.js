0685b837a9cea9764a547bc772884af003c9b25a
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-03-19 05:32:12 +0000

    constの挙動確認

--- src/index.js
+++ src/index.js
@@ -13,12 +13,22 @@
 // var val1 = 'var変数は再宣言可能';
 // console.log(val1);
 
-let val2 = 'let変数';
-console.log(val2);
+// let val2 = 'let変数';
+// console.log(val2);
 
-// letは上書き可能
-val2 = 'let変数を上書き';
-console.log(val2);
+// // letは上書き可能
+// val2 = 'let変数を上書き';
+// console.log(val2);
 
-// letは再宣言不可能
-let val2 = 'letは再宣言不可能';
+// // letは再宣言不可能
+// let val2 = 'letは再宣言不可能';
+
+const val3 = 'const変数';
+console.log(val3);
+
+// const変数は上書き不可能
+val3 = 'const変数上書き';
+console.log(val3);
+
+// const変数は再宣言不可能
+const val3 = 'const変数を再宣言';
